451cb5d25cf3acba9b6e46c282efc53c4b3bf1ad
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:22:21 +0530

    Welcome Code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackQueueLinkedList
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Stack Queue Problem..");
+        }
+    }
+}
