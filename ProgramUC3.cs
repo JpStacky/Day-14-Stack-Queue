e6645f6f024097e37dfe9997b61f1a083ab905ba
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:27:53 +0530

    Queue Insertion

---
+++ ProgramUC3.cs
@@ -0,0 +1,14 @@
+﻿namespace StackQueueLinkedList
+{
+    public class ProgramUC3
+    {
+        public static void Main()
+        {
+            QueueUC3 queue = new QueueUC3();
+            queue.Initialize();
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Enqueue(56);
+        }
+    }
+}
---
+++ QueueUC3.cs
@@ -0,0 +1,15 @@
+﻿namespace StackQueueLinkedList
+{
+    public class QueueUC3
+    {
+        LinkedList<int> list;
+        public void Initialize()
+        { 
+            list = new LinkedList<int>();
+        }
+        public void Enqueue(int n)
+        {
+            list.Append(n);
+        }
+    }
+}
