4992c797b4081b36303157b2988f72a1dbd13bb0
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:29:11 +0530

    Queue Dequeue

---
+++ ProgramUC4.cs
@@ -0,0 +1,15 @@
+﻿namespace StackQueueLinkedList
+{
+    public class ProgramUC4
+    {
+        public static void Main()
+        {
+            QueueUC4 queue = new QueueUC4();
+            queue.Initialize();
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Enqueue(56);
+            queue.Dequeue();
+        }
+    }
+}
---
+++ QueueUC4.cs
@@ -0,0 +1,24 @@
+﻿namespace StackQueueLinkedList
+{
+    public class QueueUC4
+    {
+        LinkedList<int> list;
+        public void Initialize()
+        {
+            list = new LinkedList<int>();
+        }
+        public void Enqueue(int n)
+        {
+            list.Append(n);
+        }
+        public void Dequeue()
+        {
+            foreach (var num in list)
+            {
+                Console.WriteLine(num);
+                //Console.WriteLine("Removing Element " + num + " from Queue ");
+               // list.Remove(num);
+            }
+        }
+    }
+}
