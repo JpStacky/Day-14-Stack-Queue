c163949c3763a046773f7d95b3e915979107e9cc
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:24:25 +0530

    Stack Insertion

---
+++ ProgramUC1.cs
@@ -0,0 +1,16 @@
+﻿using StackQueueLinkedList;
+
+namespace StackQueueLinkedList
+{
+    class ProgramUC1
+    {
+        public static void Main()
+        {
+            StackUC1 stack = new StackUC1();
+            stack.Initialize();
+            stack.Add(70);
+            stack.Add(30);
+            stack.Add(56);
+        }
+    }
+}
---
+++ StackUC1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueLinkedList
+{
+    public class StackUC1
+    {
+        LinkedList<int> list;
+        public void Initialize()
+        {
+            list = new LinkedList<int>();
+        }
+        public void Add(int n)
+        {
+            list.AddLast(n);
+        }
+    }
+}
