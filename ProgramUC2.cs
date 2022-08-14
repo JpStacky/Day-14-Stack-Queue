90f7117f552a751b168f2f27e64a9acfd5b244a8
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:25:47 +0530

    Stack peak and pop

---
+++ ProgramUC2.cs
@@ -0,0 +1,17 @@
+﻿using StackQueueLinkedList;
+
+namespace StackQueueLinkedList
+{
+    class ProgramUC2
+    {
+        public static void Main()
+        {
+            StackUC2 stack = new StackUC2();
+            stack.Initialize();
+            stack.Add(70);
+            stack.Add(30);
+            stack.Add(56);
+            stack.PeakPop();
+        }
+    }
+}
---
+++ StackUC2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueLinkedList
+{
+    public class StackUC2
+    {
+        LinkedList<int> list;
+ 
+        public void Initialize()
+        {
+            list = new LinkedList<int>();
+        }
+        public void Add(int n)
+        {
+            list.AddLast(n);
+        }
+        public void PeakPop()
+        {
+            foreach (int num in list.ToList())
+            {
+                Console.WriteLine("Removing element "+num+" from Linked List...");
+                list.Remove(num);
+            }
+        }
+    }
+}
